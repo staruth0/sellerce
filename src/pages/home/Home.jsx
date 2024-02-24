b3ead719d52aa4efa675ab77600ece6974579bf5
EnowMyke-A <enowmyke944@gmail.com>
2024-02-24 18:04:48 +0100

    update

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "swiper/css";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/free-mode';
+import HeroComponent from "./components/Hero";
+import PopularComponent from "./components/PopularPoducts";
+import PopularCategory from "./components/PopularCategory";
+import './components/home.css'
+
+const HomeComponent = ()=>{
+    return(
+        <div className="main-home-container">
+            <HeroComponent/>
+            <PopularComponent/>
+            <PopularCategory/>
+        </div>
+    )
+}
+
+export default HomeComponent;
